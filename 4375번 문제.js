bb4417dd0491842fc6250a20a18f96865707ff9b
Hong sung soo <sow9707@daum.net>
2024-02-08 20:19:39 +0900

    4375번 문제: 문제 해결 처음 풀었던 과정에선 i의 단위가 너무 커져서 시간초과 오류가 떳는데 모듈러 연산의 분배법칙으로 i의 단위를 줄이는 과정을 통해 해결할 수 있었다

--- 4375번 문제.js
+++ 4375번 문제.js
@@ -1,11 +1,11 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let result = input.map((input) => {
   let i = 1;
   let count = 1;
   while (i % input !== 0) {
-    i += 10 ** count;
+    i = (i * 10 + 1) % input;
     count++;
   }
   return count;
