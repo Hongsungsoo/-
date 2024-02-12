f5a54dc92b80bd20f62fa9d96b5d9681a9b51487
Hong sung soo <sow9707@daum.net>
2024-02-13 05:15:15 +0900

    17427번 문제: 문제 해결 자연수N이하의 자연수의 약수의 총합을 구하려 하지 말고 N이하의 자연수들이 약수로서 몇번 더해지는지 구하고 그것의 총합을 구한 방식으로 해결함

--- 17427번 문제.js
+++ 17427번 문제.js
@@ -1,24 +1,10 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim();
+let input = Number(fs.readFileSync('/dev/stdin').toString().trim());
 
-function sum(y) {
-  let number = parseInt(y);
-  let totalSum = 0;
-  for (let i = 0; i < number + 1; i++) {
-    if (number % i === 0) {
-      totalSum += i;
-    }
-  }
-  return totalSum;
+let result = 0;
+
+for (let i = 1; i <= input; i++) {
+  result += Math.floor(input / i) * i;
 }
 
-function g(x) {
-  let number = parseInt(x);
-  let result = 0;
-  for (let i = 0; i < number + 1; i++) {
-    result += sum(i);
-  }
-  console.log(result);
-}
-
-g('10');
+console.log(result);
